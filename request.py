75ebcbe85b11e1fc3d857b99a01783fa4b6b4564
luisFelipeFreitas <luisfreitas@id.uff.br>
2017-07-13 19:18:44 -0300

    Add files via upload

--- request.py
+++ request.py
@@ -1,16 +1,9 @@
-#!/bin/python2
-
-#__author__ = 'Luis Felipe'
 import sys
 
 from bs4 import BeautifulSoup # $ pip install beautifulsoup4
 
-
-#url = sys.argv[1]
 soup = BeautifulSoup(open(sys.argv[1]),"lxml")
-print(soup.title)
-
-#
-#for text in soup.find_all('p'):
-#    print(text.string)
-#print(soup.p)
+palavras= soup.title.string
+for plavavra in palavras:
+    palavra=palavra.encode("cp1250")
+    print (palavra.decode('unicode_escape').encode('ascii','ignore'))
