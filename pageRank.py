57e26ce1f65efad0a940288e34dc2dc02217799b
petrolifero <joaopedroabreudesouza@cryptolab.net>
2017-08-03 18:27:15 -0300

    Update pageRank.py

--- pageRank.py
+++ pageRank.py
@@ -2,9 +2,13 @@ import networkx
 
 
 g = nx.MultiDiGraph()
-entrada=input()
-while (entrada != ""):
-  entrada=entrada.split()
-  g.addEdge(entrada[0],entrada[1]);
+
+while (True):
+  try:
+    entrada=input()
+    entrada=entrada.split()
+    g.addEdge(entrada[0],entrada[1]);
+  except Exception:
+    break
   entrada=input()
 d = pagerank(g, alpha=0.85, max_iter=1000, tol=1e-06)
