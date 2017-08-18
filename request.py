3d3635955dcac5bffa2f08b60f070dbe897e3afc
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2017-08-18 08:50:52 -0300

    add exception handler

--- request.py
+++ request.py
@@ -2,8 +2,12 @@ import sys
 
 from bs4 import BeautifulSoup # $ pip install beautifulsoup4
 
-soup = BeautifulSoup(open(sys.argv[1]),"lxml")
-palavras= soup.title.string
-for plavavra in palavras:
-    palavra=palavra.encode("cp1250")
-    print (palavra.decode('unicode_escape').encode('ascii','ignore'))
+
+try:
+	soup = BeautifulSoup(open(sys.argv[1]),"lxml")
+	palavras= soup.title.string
+	for plavavra in palavras:
+		palavra=palavra.encode("cp1250")
+		print (palavra.decode('unicode_escape').encode('ascii','ignore'))
+except IndexError:
+	print("DOn't have the path of file",file=sys.stderr)
