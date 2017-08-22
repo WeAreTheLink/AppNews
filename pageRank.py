a927c5afce090be3ac4088a4d69a886bde59496f
joao pedro abreu de souza <joaopedroabreudesouza@cryptolab.net>
2017-08-22 17:58:37 -0300

    first training code written, just need data to see the erros and after we will divide the data in two sets : 90/10

--- pageRank.py
+++ pageRank.py
@@ -45,6 +45,43 @@ net = buildnetwork(n,n,n,1, bias=True)
 
 dataSet=SupervisedDataSet(n,1)
 
+#here, we need data/bagOfWords with the bag of words separated by your pageRank by a '#'
+
+##Something like :
+#   1 0 5 28 234 54#0.65
+#   2 8 2 0 0 9#0.98
+
+fp=open("data/bagOfWords","r")
+
+for line in fp:
+    line=line.split('#')
+    bag,pageRank = line
+    pagerank=int(pagerank)
+    bag=bag.split()
+    dataSet.addSample(tuple(bag),pagerank)
+
+
+#this is a preliminar study. Because normally we need two divisions : training set and verify set
+
+from pybrain.supervised.trainers import BackpropTrainer
+
+trainer = BackpropTrainer(net, dataSet)
+
+error=trainer.trainUntilConvergence()
+
+print error
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
